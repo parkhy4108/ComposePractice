e78fe41d9724f50df548cbef2ed791b57fb29ee0
parkhy4108 <parkhy4108@gmail.com>
2023-10-17 21:57:39 +0900

    second

--- app/src/main/java/com/devstitch/composepractice/MainActivity.kt
+++ app/src/main/java/com/devstitch/composepractice/MainActivity.kt
@@ -105,8 +105,7 @@ class MainActivity : ComponentActivity() {
                         ) {
 //                            PosterAnimation()
                         }
-                        /////추가
-                        /////추가2
+
                     }
 //                    animationPractice1()
 //                    ComposeLine()
